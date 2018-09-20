abded8d421435f6cb20be2d8abe659218cba9425
zhangfb <zhangfb@shishike.com>
2018-09-20 17:29:06 +0800

    增加demo，wait/notify

---
+++ src/main/java/com/wtuoblist/beyond/concurrent/demo/SimpleWN.java
@@ -0,0 +1,48 @@
+package com.wtuoblist.beyond.concurrent.demo;
+
+/**
+ * @author zhangfb
+ */
+public class SimpleWN {
+
+    private static final Object obj = new Object();
+
+    public static class T1 extends Thread {
+
+        @Override public void run() {
+            synchronized (obj) {
+                System.out.println(System.currentTimeMillis() + ":T1 start!");
+                try {
+                    System.out.println(System.currentTimeMillis() + ":T1 wait for object");
+                    obj.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(System.currentTimeMillis() + ":T1 end");
+            }
+        }
+    }
+
+    public static class T2 extends Thread {
+
+        @Override public void run() {
+            synchronized (obj) {
+                System.out.println(System.currentTimeMillis() + ":T2 start! notify one thread");
+                obj.notify();
+                System.out.println(System.currentTimeMillis() + ":T2 end");
+                try {
+                    Thread.sleep(2000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new T1();
+        Thread t2 = new T2();
+        t1.start();
+        t2.start();
+    }
+}
