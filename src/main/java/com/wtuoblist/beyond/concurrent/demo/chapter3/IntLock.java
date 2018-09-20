585ced8dbb30cb3f342a2ed2f51286da8a4b990c
zhangfb <zhangfb@shishike.com>
2018-09-20 21:48:52 +0800

    增加demo，ReentrantLock可中断

---
+++ src/main/java/com/wtuoblist/beyond/concurrent/demo/chapter3/IntLock.java
@@ -0,0 +1,62 @@
+package com.wtuoblist.beyond.concurrent.demo.chapter3;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author zhangfb
+ */
+public class IntLock implements Runnable {
+
+    public static ReentrantLock lock1 = new ReentrantLock();
+    public static ReentrantLock lock2 = new ReentrantLock();
+    int lock;
+
+    public IntLock(int lock) {
+        this.lock = lock;
+    }
+
+    public void run() {
+        try {
+            if (lock == 1) {
+                lock1.lockInterruptibly();
+                try {
+                    Thread.sleep(500L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                lock2.lockInterruptibly();
+            } else {
+                lock2.lockInterruptibly();
+                try {
+                    Thread.sleep(500L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                lock1.lockInterruptibly();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (lock1.isHeldByCurrentThread()) {
+                lock1.unlock();
+            }
+            if (lock2.isHeldByCurrentThread()) {
+                lock2.unlock();
+            }
+            System.out.println(Thread.currentThread().getId() + ":thread exit");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        IntLock r1 = new IntLock(1);
+        IntLock r2 = new IntLock(2);
+
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+        t1.start();
+        t2.start();
+        Thread.sleep(1000L);
+
+        t2.interrupt();
+    }
+}
