4b5bd6a3235c32d8357eee31d48a9c59eaea8fca
zhangfb <zhangfb@shishike.com>
2018-09-20 22:07:03 +0800

    增加demo，ReentrantLock try lock

---
+++ src/main/java/com/wtuoblist/beyond/concurrent/demo/chapter3/TryLock.java
@@ -0,0 +1,72 @@
+package com.wtuoblist.beyond.concurrent.demo.chapter3;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author zhangfb
+ */
+public class TryLock implements Runnable {
+
+    public static ReentrantLock lock1 = new ReentrantLock();
+    public static ReentrantLock lock2 = new ReentrantLock();
+    int lock;
+
+    public TryLock(int lock) {
+        this.lock = lock;
+    }
+
+    public void run() {
+        if (lock == 1) {
+            while (true) {
+                if (lock1.tryLock()) {
+                    try {
+                        try {
+                            Thread.sleep(500L);
+                        } catch (InterruptedException e) {
+                        }
+                        if (lock2.tryLock()) {
+                            try {
+                                System.out.println(Thread.currentThread().getId() + ":My Job done");
+                                return;
+                            } finally {
+                                lock2.unlock();
+                            }
+                        }
+                    } finally {
+                        lock1.unlock();
+                    }
+                }
+            }
+        } else {
+            while (true) {
+                if (lock2.tryLock()) {
+                    try {
+                        try {
+                            Thread.sleep(500L);
+                        } catch (InterruptedException e) {
+                        }
+                        if (lock1.tryLock()) {
+                            try {
+                                System.out.println(Thread.currentThread().getId() + ":My Job done");
+                                return;
+                            } finally {
+                                lock1.unlock();
+                            }
+                        }
+                    } finally {
+                        lock2.unlock();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TryLock r1 = new TryLock(1);
+        TryLock r2 = new TryLock(2);
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+        t1.start();
+        t2.start();
+    }
+}
