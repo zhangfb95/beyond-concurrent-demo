b55957602c336585e36642b50797e9c79f148086
zhangfb <zhangfb@shishike.com>
2018-09-20 10:41:04 +0800

    增加safe stop和unSafe stop的demo

---
+++ src/main/java/com/wtuoblist/beyond/concurrent/demo/StopThreadUnsafe.java
@@ -0,0 +1,86 @@
+package com.wtuoblist.beyond.concurrent.demo;
+
+/**
+ * @author zhangfb
+ */
+public class StopThreadUnsafe {
+
+    public static User u = new User();
+
+    public static class User {
+
+        private int id;
+        private String name;
+
+        public User() {
+            this.id = 0;
+            this.name = "0";
+        }
+
+        @Override public String toString() {
+            return "User{" +
+                   "id=" + id +
+                   ", name='" + name + '\'' +
+                   '}';
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class ChangeObjectThread extends Thread {
+
+        @Override public void run() {
+            while (true) {
+                synchronized (u) {
+                    int v = (int) (System.currentTimeMillis() / 1000);
+                    u.setId(v);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    u.setName(v + "");
+                    Thread.yield();
+                }
+            }
+        }
+    }
+
+    public static class ReadObjectThread extends Thread {
+
+        @Override public void run() {
+            while (true) {
+                synchronized (u) {
+                    if (u.getId() != Integer.parseInt(u.getName())) {
+                        System.out.println(u.toString());
+                    }
+                }
+                Thread.yield();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new ReadObjectThread().start();
+        while (true) {
+            Thread t = new ChangeObjectThread();
+            t.start();
+            Thread.sleep(150);
+            t.stop();
+        }
+    }
+}
