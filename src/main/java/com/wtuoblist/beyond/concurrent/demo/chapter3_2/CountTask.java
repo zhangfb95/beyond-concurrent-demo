b7c8c02c6c60653162a6302a21a1068c9c24b26c
zhangfb <zhangfb@shishike.com>
2018-09-21 17:50:40 +0800

    增加demo，forkjoin

---
+++ src/main/java/com/wtuoblist/beyond/concurrent/demo/chapter3_2/CountTask.java
@@ -0,0 +1,64 @@
+package com.wtuoblist.beyond.concurrent.demo.chapter3_2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * @author zhangfb
+ */
+public class CountTask extends RecursiveTask<Long> {
+
+    private static final int THRESHOLD = 10000;
+    private long start;
+    private long end;
+
+    public CountTask(long start, long end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override protected Long compute() {
+        long sum = 0;
+        boolean canCompute = (end - start) < THRESHOLD;
+        if (canCompute) {
+            for (long i = start; i <= end; i++) {
+                sum += i;
+            }
+        } else {
+            long step = (start + end) / 100;
+            List<CountTask> subTasks = new ArrayList<>();
+            long pos = start;
+            for (int i = 0; i < 100; i++) {
+                long lastOne = pos + step;
+                if (lastOne > end) {
+                    lastOne = end;
+                }
+                CountTask subTask = new CountTask(pos, lastOne);
+                pos += step + 1;
+                subTasks.add(subTask);
+                subTask.fork();
+            }
+            for (CountTask subTask : subTasks) {
+                sum += subTask.join();
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        CountTask countTask = new CountTask(0, 200000L);
+        ForkJoinTask<Long> result = forkJoinPool.submit(countTask);
+        try {
+            System.out.println("sum=" + result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
