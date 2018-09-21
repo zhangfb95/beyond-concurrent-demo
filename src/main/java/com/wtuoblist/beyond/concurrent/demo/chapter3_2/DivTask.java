0a61bce782e2e907e0b936cf0ad5d5a0a53624fb
zhangfb <zhangfb@shishike.com>
2018-09-21 17:22:45 +0800

    增加demo，线程池吞并异常

---
+++ src/main/java/com/wtuoblist/beyond/concurrent/demo/chapter3_2/DivTask.java
@@ -0,0 +1,30 @@
+package com.wtuoblist.beyond.concurrent.demo.chapter3_2;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zhangfb
+ */
+public class DivTask implements Runnable {
+
+    int a, b;
+
+    public DivTask(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void run() {
+        System.out.println("c is: " + a / b);
+    }
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor pools = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+                                                          0L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+        for (int i = 0; i < 5; i++) {
+            pools.submit(new DivTask(100, i));
+        }
+    }
+}
