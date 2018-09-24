ffadde5c7f748fcbe45a4b8a23fc46b1f7fabeac
zhangfb <zhangfb@shishike.com>
2018-09-24 20:07:14 +0800

    增加demo，fix AtomicInteger过程不会记录的问题

--- src/main/java/com/wtuoblist/beyond/concurrent/demo/chapter4_4/AtomicStampedReferenceDemo.java
+++ src/main/java/com/wtuoblist/beyond/concurrent/demo/chapter4_4/AtomicStampedReferenceDemo.java
@@ -1,28 +1,25 @@
 package com.wtuoblist.beyond.concurrent.demo.chapter4_4;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * @author zhangfb
  */
-public class AtomicIntegerQuestionDemo {
+public class AtomicStampedReferenceDemo {
 
-    static AtomicReference<Integer> money = new AtomicReference<>();
-
-    static {
-        money.set(19);
-    }
+    static AtomicStampedReference<Integer> money = new AtomicStampedReference<>(19, 0);
 
     void add() {
         for (int i = 0; i < 30; i++) {
+            final int timestamp = money.getStamp();
             new Thread() {
                 @Override public void run() {
                     while (true) {
                         while (true) {
-                            Integer m = money.get();
+                            Integer m = money.getReference();
                             if (m < 20) {
-                                if (money.compareAndSet(m, m + 20)) {
-                                    System.out.println("余额小于20元，充值成功，余额：" + money.get() + "元");
+                                if (money.compareAndSet(m, m + 20, timestamp, timestamp + 1)) {
+                                    System.out.println("余额小于20元，充值成功，余额：" + money.getReference() + "元");
                                     break;
                                 }
                             } else {
@@ -41,11 +38,12 @@ public class AtomicIntegerQuestionDemo {
             @Override public void run() {
                 for (int i = 0; i < 100; i++) {
                     while (true) {
-                        Integer m = money.get();
+                        int timestamp = money.getStamp();
+                        Integer m = money.getReference();
                         if (m > 10) {
                             System.out.println("大于10元");
-                            if (money.compareAndSet(m, m - 10)) {
-                                System.out.println("成功消费10元，余额：" + money.get());
+                            if (money.compareAndSet(m, m - 10, timestamp, timestamp + 1)) {
+                                System.out.println("成功消费10元，余额：" + money.getReference());
                                 break;
                             }
                         } else {
@@ -64,8 +62,8 @@ public class AtomicIntegerQuestionDemo {
     }
 
     public static void main(String[] args) {
-        new AtomicIntegerQuestionDemo().consume();
-        new AtomicIntegerQuestionDemo().add();
+        new AtomicStampedReferenceDemo().consume();
+        new AtomicStampedReferenceDemo().add();
     }
 
 }
